5ca9503f66851c783a8410624d9235d250544cbb
BhaveshMP <115444272+BhaveshMP@users.noreply.github.com>
2024-11-25 11:55:07 +0000

    started events

--- 07_projects/04_GuessTheNumber/script.js
+++ 07_projects/04_GuessTheNumber/script.js
@@ -1,19 +1,49 @@
 
 const form = document.querySelector("form")
+const min = 1
+const max = 100
 
-const rnum = Math.floor((Math.random() * 10 + 1))
-console.log(rnum)
+const giveRandom = () => {
+    return Math.floor(((Math.random() * (max - min)) + 1))
+    }
+let rnum = giveRandom() 
+console.log('random num: ',rnum)
 
-const attempt = 10
-const guesses = document.getElementById("guesses")
-const lastResult = document.getElementById("lastResult")
+let attempt = 10
+
+const winOrloose = document.querySelector(".lowOrHi")
+let guesses = document.querySelector(".guesses")
+const lastResult = document.querySelector(".lastResult")
+let lastResultArr = []
+
+console.log(attempt)
 
 form.addEventListener('submit',function(e){
     e.preventDefault()
 
     const num = parseInt(document.getElementById("guessField").value)
+    lastResultArr.push(num)
     attempt--
+    console.log(attempt)
+    guesses.innerHTML = lastResultArr
+    lastResult.innerHTML = attempt
+
+    let status = ""
+    if(num==rnum || attempt == 0){ 
+    
     if(num==rnum){
-        console.log('win')
+        status = 'WIN !!!!!!!!!!!'
+    }else if(attempt == 0){
+        status = 'LOSS ??????';
     }
+        console.log(status)
+        attempt = 10
+        lastResult.innerHTML = 10
+        guesses.innerHTML = ""
+        lastResultArr = []
+        rnum = giveRandom() 
+        console.log('random num: ',rnum)
+    }
+    winOrloose.innerHTML = status
+    
 })
